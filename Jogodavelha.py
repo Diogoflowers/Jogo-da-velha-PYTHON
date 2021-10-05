a00dbe1063c27338e2f6e417a330ba0e5d2411c1
Diogo Flores <diogoflores2009@gmail.com>
2021-10-05 20:42:29 -0300

    Update Jogodavelha.py

--- Jogodavelha.py
+++ Jogodavelha.py
@@ -1,103 +1,80 @@
 jogovelha = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 
-def jogo():
-    global jogovelha
+def Tabela():
     print('                                                            │     │')
-    print(f'                                                        {jogovelha[0]}   │  {jogovelha[1]}  │   {jogovelha[2]}')
+    print(
+        f'                                                        {jogovelha[0]}   │  {jogovelha[1]}  │   {jogovelha[2]}')
     print(f'                                                     _______│_____│______')
     print('                                                            │     │')
-    print(f'                                                        {jogovelha[3]}   │  {jogovelha[4]}  │   {jogovelha[5]}')
+    print(
+        f'                                                        {jogovelha[3]}   │  {jogovelha[4]}  │   {jogovelha[5]}')
     print(f'                                                     _______│_____│______')
-    print(f'                                                        {jogovelha[6]}   │  {jogovelha[7]}  │   {jogovelha[8]}')
+    print(
+        f'                                                        {jogovelha[6]}   │  {jogovelha[7]}  │   {jogovelha[8]}')
     print('                                                            │     │ ')
-    print('\n'*8)
+    print('\n' * 8)
 
 
-def vitoria():
-    if jogovelha[0] == 'X' and jogovelha[3] == 'X' and jogovelha[6] == 'X':
-        print('Jogador X ganhou!')
+def Verifica_Vitoria(simbolo):
+    Tabela()
+    if jogovelha[0] == simbolo and jogovelha[3] == simbolo and jogovelha[6] == simbolo:
+        print(f'Jogador {simbolo} ganhou!')
         exit()
-    if jogovelha[1] == 'X' and jogovelha[4] == 'X' and jogovelha[7] == 'X':
-        print('Jogador X ganhou!')
+    if jogovelha[1] == simbolo and jogovelha[4] == simbolo and jogovelha[7] == simbolo:
+        print(f'Jogador {simbolo} ganhou!')
         exit()
-    if jogovelha[2] == 'X' and jogovelha[5] == 'X' and jogovelha[8] == 'X':
-        print('Jogador X ganhou!')
+    if jogovelha[2] == simbolo and jogovelha[5] == simbolo and jogovelha[8] == simbolo:
+        print(f'Jogador {simbolo} ganhou!')
         exit()
-    if jogovelha[0] == 'X' and jogovelha[1] == 'X' and jogovelha[2] == 'X':
-        print('Jogador X ganhou!')
+    if jogovelha[0] == simbolo and jogovelha[1] == simbolo and jogovelha[2] == simbolo:
+        print(f'Jogador {simbolo} ganhou!')
         exit()
-    if jogovelha[3] == 'X' and jogovelha[4] == 'X' and jogovelha[5] == 'X':
-        print('Jogador X ganhou!')
+    if jogovelha[3] == simbolo and jogovelha[4] == simbolo and jogovelha[5] == simbolo:
+        print(f'Jogador {simbolo} ganhou!')
         exit()
-    if jogovelha[6] == 'X' and jogovelha[7] == 'X' and jogovelha[8] == 'X':
-        print('Jogador X ganhou!')
+    if jogovelha[6] == simbolo and jogovelha[7] == simbolo and jogovelha[8] == simbolo:
+        print(f'Jogador {simbolo} ganhou!')
         exit()
-    if jogovelha[2] == 'X' and jogovelha[4] == 'X' and jogovelha[6] == 'X':
-        print('Jogador X ganhou!')
+    if jogovelha[2] == simbolo and jogovelha[4] == simbolo and jogovelha[6] == simbolo:
+        print(f'Jogador {simbolo} ganhou!')
         exit()
-    if jogovelha[0] == 'X' and jogovelha[4] == 'X' and jogovelha[8] == 'X':
-        print('Jogador X ganhou!')
-        exit()
-    if jogovelha[0] == 'O' and jogovelha[3] == 'O' and jogovelha[6] == 'O':
-        print('Jogador O ganhou!')
-        exit()
-    if jogovelha[1] == 'O' and jogovelha[4] == 'O' and jogovelha[7] == 'O':
-        print('Jogador O ganhou!')
-        exit()
-    if jogovelha[2] == 'O' and jogovelha[5] == 'O' and jogovelha[8] == 'O':
-        print('Jogador O ganhou!')
-        exit()
-    if jogovelha[0] == 'O' and jogovelha[1] == 'O' and jogovelha[2] == 'O':
-        print('Jogador O ganhou!')
-        exit()
-    if jogovelha[3] == 'O' and jogovelha[4] == 'O' and jogovelha[5] == 'O':
-        print('Jogador O ganhou!')
-        exit()
-    if jogovelha[6] == 'O' and jogovelha[7] == 'O' and jogovelha[8] == 'O':
-        print('Jogador O ganhou!')
-        exit()
-    if jogovelha[2] == 'O' and jogovelha[4] == 'O' and jogovelha[6] == 'O':
-        print('Jogador O ganhou!')
-        exit()
-    if jogovelha[0] == 'O' and jogovelha[4] == 'O' and jogovelha[8] == 'O':
-        print('Jogador O ganhou!')
+    if jogovelha[0] == simbolo and jogovelha[4] == simbolo and jogovelha[8] == simbolo:
+        print(f'Jogador {simbolo} ganhou!')
         exit()
 
 
-jogo()
+jogador = input('Você quer ser X ou O?').upper()
 contador = 0
 while True:
-    while True:
-        while True:
-            escolha = int(input('Jogada X: '))
-            if escolha == escolha >= 1 and escolha <= 9:
-                break
+    if jogador == 'X':
+        while jogador == 'X':
+            jogada = int(input(f'Jogada {jogador}: (Apenas de 1 até 9)'))
+            if jogada == jogada >= 1 and jogada <= 9:
+                if jogovelha[jogada - 1] == ' ':
+                    jogovelha[jogada - 1] = 'X'
+                    Verifica_Vitoria(jogador)
+                    jogador = 'O'
+                    contador += 1
+                else:
+                    Tabela()
+                    print('Posição já ocupada')
             else:
-                print('Apenas casa de 1 até 9')
-        if jogovelha[escolha - 1] == ' ':
-            contador += 1
-            jogovelha[escolha - 1] = 'X'
-            jogo()
-            vitoria()
-            break
-        else:
-            print('Essa casa já está selecionada')
+                Tabela()
     if contador == 9:
-        print('EMPATOU')
-        break
-    while True:
-        while True:
-            escolha = int(input('Jogada O: '))
-            if escolha == escolha >= 1 and escolha <= 9:
-                break
+        print('EMPATOU!')
+        exit()
+    if jogador == 'O':
+        while jogador == 'O':
+            jogada = int(input(f'Jogada {jogador}: (Apenas de 1 até 9)'))
+            if jogada == jogada >= 1 and jogada <= 9:
+                if jogovelha[jogada - 1] == ' ':
+                    jogovelha[jogada - 1] = 'O'
+                    Verifica_Vitoria(jogador)
+                    jogador = 'X'
+                    contador += 1
+                else:
+                    Tabela()
+                    print('Posição já ocupada')
             else:
-                print('Apenas casa de 1 até 9')
-        if jogovelha[escolha - 1] == ' ':
-            contador += 1
-            jogovelha[escolha - 1] = 'O'
-            jogo()
-            vitoria()
-            break
-        else:
-            print('Essa casa já está selecionada')
+                Tabela()
